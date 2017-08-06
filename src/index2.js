81e3e6573e847be3eb274782ea67474eeb4ab9cc
joeyguo <lgsincy@gmail.com>
2017-08-06 21:49:01 +0800

    new index

---
+++ src/index2.js
@@ -0,0 +1,11 @@
+import Stage from './stage.js'
+import Track from './trackjs.js'
+import Frames from './framesjs.js'
+import loadImage from './loadImage.js'
+
+export default {
+    Stage,
+    Track,
+    Frames,
+    loadImage,
+}
