7713bd9cf238962e9eb26daf5fc95c6001849dcc
joeyguo <lgsincy@gmail.com>
2017-08-15 23:51:29 +0800

    update

--- example/track-sprites/index.js
+++ example/track-sprites/index.js
@@ -46,16 +46,6 @@ img.onload = () => {
     var material1 = new Frame(null, data, "forward", 3);
     var material2 = new Frame(null, data, "back", 3);
 
-    material1
-        .onFrame(function(i){
-            console.log("material1");
-        });
-
-    material2
-        .onFrame(function(i){
-            console.log("material2");
-        });
-        
     track.add([material1, material2]);
 
     setInterval(()=> {
